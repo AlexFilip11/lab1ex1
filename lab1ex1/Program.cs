32b3768e7199cfcafda0555ff1d7c62ed2ed6144
Alex Filip <alexfilippaul@gmail.com>
2022-11-27 17:09:51 +0200

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab1ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Volum paralelipiped dreptunghic
+            int lungime, latime, inaltime, volum;
+            Console.WriteLine("Introduceti lungimea: ");
+            lungime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti latimea: ");
+            latime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti inaltimea: ");
+            inaltime = int.Parse(Console.ReadLine());
+            volum = lungime * latime * inaltime;
+            Console.WriteLine("Volumul paralelipipedului este: " + volum);
+        }
+    }
+}
